57b73af4a8f37433e810ea986582bc8b1fd06556
Lunanarch <luna-thom@riseup.net>
2023-06-27 15:59:17 +0200

    Ajout du site

---
+++ views/includes/navbar.php
@@ -0,0 +1,130 @@
+<nav class="navbar is-fixed-top" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item">
+      <img src="assets/images/logo.png" width="28" height="28" alt="Logo de Inakaz représentant un chat dans un carton">&nbsp<strong><p class="title is-4">Inakaz</p></strong>
+    </a>
+
+    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbar">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbar" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item" href="https://inakaz.fr/home">
+      <i class="fas fa-home" style="color: black"></i>&nbspAccueil
+      </a>
+       
+      <!-- Services -->
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link" href="https://inakaz.fr/services">
+        <i class="fas fa-server" style="color: black"></i>&nbspServices
+        </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            InaPad
+          </a>
+          <a class="navbar-item">
+            InaCalc
+          </a>
+          <a class="navbar-item">
+            InaDiag
+          </a>
+          <a class="navbar-item">
+            InaMind
+          </a>
+          <a class="navbar-item">
+            InaPoll
+          </a>
+          <a class="navbar-item">
+            InaPaste
+          </a>
+          <a class="navbar-item">
+            InaLink
+          </a>
+          <a class="navbar-item">
+            InaWhiteboard
+          </a>
+          <a class="navbar-item">
+            InaRSS
+          </a>
+          <a class="navbar-item">
+            InaShare
+          </a>
+          <a class="navbar-item">
+            InaSearch
+          </a>
+          <a class="navbar-item">
+            InaPlume
+          </a>
+          <a class="navbar-item">
+            InaTube
+          </a>
+          <a class="navbar-item">
+            InaTrix
+          </a>
+          <a class="navbar-item">
+            Nitter
+          </a>
+          <hr class="navbar-divider">
+          <a class="navbar-item">
+            InaCloud
+          </a>
+          <a class="navbar-item">
+            InaMail
+          </a>
+          <a class="navbar-item">
+            InaChat
+          </a>
+        </div>
+      </div>
+
+      <a class="navbar-item" href="https://wiki.inakaz.fr/">
+      <i class="fas fa-book" style="color: black"></i>&nbspWiki
+      </a>
+
+      <a class="navbar-item" href="https://blog.inakaz.fr/">
+      <i class="fas fa-book" style="color: black"></i>&nbspBlog
+      </a>
+
+      <!-- Autres -->
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link">
+        <i class="fas fa-ellipsis-h"></i></a>
+        <div class="navbar-dropdown">
+          <a class="navbar-item">
+            A propos
+          </a>
+          <a class="navbar-item">
+            Mentions légales
+          </a>
+          <a class="navbar-item">
+            Wiki
+          </a>
+          <hr class="navbar-divider">
+          <a class="navbar-item">
+            Report an issue
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-link is-light is-rounded">
+            <strong><i class="far fa-envelope-open"></i>&nbspContacter</strong>
+          </a>
+          <a class="button is-link is-rounded" href="https://inakaz.fr/yunohost/sso">
+            <strong><i class="fas fa-user-circle"></i>&nbspEspace Adhérent·e</strong>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
